1d3728926c32b80f30f1ef231eec5ee9027dcbd7
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-04-12 11:33:43 -0300

    Criei atualizar filme no DAO

--- src/ads/pipoca/model/dao/FilmeDAO.java
+++ src/ads/pipoca/model/dao/FilmeDAO.java
@@ -80,4 +80,36 @@ public class FilmeDAO {
 		}
 		return filme;
 	}
+	
+	//RASCUNHO
+	public Filme atualizarFilme(Filme filme) throws IOException {
+		
+		String sql = "UPDATE filme "
+				+ "SET titulo= ?, descricao= ?,diretor=?,posterpath=?, popularidade=?,data_lancamento=?,id_genero=? "
+				+ "WHERE id= ?";
+			
+		try (Connection conn = ConnectionFactory.getConnection(); PreparedStatement pst = conn.prepareStatement(sql);) {
+
+			pst.setString(1, filme.getTitulo());
+			pst.setString(2, filme.getDescricao());
+			pst.setString(3, filme.getDiretor());
+			pst.setString(4, filme.getPosterPath());
+			pst.setDouble(5, filme.getPopularidade());
+			pst.setDate(6, new java.sql.Date(filme.getDataLancamento().getTime()));
+			pst.setInt(7, filme.getGenero().getId());
+			pst.setInt(8, filme.getId());
+			pst.execute();
+
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IOException(e);
+		}
+		
+		Filme filmeTeste = new Filme();
+		filmeTeste = buscarFilme(filme.getId());
+		return filmeTeste;
+	}
+	//FIM DO RASCUNHO
 }
+
